2b40fe833910583c8ffb11279fed3c856357c52b
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2021-12-23 14:55:23 +0200

    Update Movie.kt

--- Movie.kt
+++ Movie.kt
@@ -12,15 +12,14 @@ data class Movie(
     val rank: Double?
 ) : Parcelable
 
-    fun getOneMovie(): Movie {
-    return Movie("Pulp Fiction", "criminal", "1995", "classic!", 8.9)
-}
 
-fun getMovieList(): List<Movie> {
-    return listOf(
+    fun getOneMovie(): Movie =
+     Movie("Pulp Fiction", "criminal", "1995", "classic!", 8.9)
+
+
+    fun getMovieList(): List<Movie> = listOf(
         Movie("Pulp Fiction", "criminal","1995", "classic!", 8.9),
         Movie("Shawshank Redemption", "prison drama", "1992","best ever", 9.2),
         Movie("City of god", "criminal" ,"2008","hard to watch",8.7),
         Movie("Titanic",  "melodrama","1998","you will cry",9.0),
         Movie("Fight Club", "drama","1999","man´s life",8.5))
-}
