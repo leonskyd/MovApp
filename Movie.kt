552cd37c2da4277126c7766eabfeaae01a90b32a
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2021-12-30 16:22:18 +0200

    Update Movie.kt

--- Movie.kt
+++ Movie.kt
@@ -5,21 +5,20 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Movie(
+    val index: Int?,
     val title: String?,
-    val genre: String, // will be changed to Array
     val release: String?,
     val overview: String?,
     val rank: Double?
 ) : Parcelable
 
-
     fun getOneMovie(): Movie =
-     Movie("Pulp Fiction", "criminal", "1995", "classic!", 8.9)
+     Movie(680,"Pulp Fiction",  "1995", "classic!", 8.9)
 
 
-    fun getMovieList(): List<Movie> = listOf(
-        Movie("Pulp Fiction", "criminal","1995", "classic!", 8.9),
-        Movie("Shawshank Redemption", "prison drama", "1992","best ever", 9.2),
-        Movie("City of god", "criminal" ,"2008","hard to watch",8.7),
-        Movie("Titanic",  "melodrama","1998","you will cry",9.0),
-        Movie("Fight Club", "drama","1999","man´s life",8.5))
+fun getMovieList(): List<Movie> = listOf(
+        Movie(680,"Pulp Fiction", "1995", "classic!", 8.9),
+        Movie(278,"Shawshank Redemption","1995","best ever", 9.2),
+        Movie(598,"City of god","2008","hard to watch",8.7),
+        Movie(597,"Titanic",  "1998","you will cry",9.0),
+        Movie(550,"Fight Club", "1999","man´s life",8.5))
